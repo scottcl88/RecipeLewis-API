e31babad59fcb710d8b4e09772040f27fde693d0
Scott Lewis <scottcl@outlook.com>
2022-05-07 14:15:53 -0400

    Renaming Models folder

---
+++ Models/UserModel.cs
@@ -0,0 +1,20 @@
+﻿namespace RecipeLewis.Models
+{
+    public class UserId
+    {
+        public int Value { get; set; }
+    }
+
+    public class UserModel : EntityDataModel
+    {
+        public UserId UserId { get; set; }
+        public Guid? UserGUID { get; set; }
+        public string? LastIPAddress { get; set; }
+        public DateTime? LastLogin { get; set; }
+        public DateTime? LastLogout { get; set; }
+        public string? TimeZone { get; set; }
+        public int? UtcOffset { get; set; }
+        public string? Email { get; set; }
+        public string? Name { get; set; }
+    }
+}
